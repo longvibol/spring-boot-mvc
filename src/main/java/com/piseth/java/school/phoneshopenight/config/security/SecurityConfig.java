1c57d671acb6e58d3c40c6718ea3959d10c183a4
longvibol <vibollong@gmail.com>
2024-08-19 16:38:48 +0700

    offi

---
+++ src/main/java/com/piseth/java/school/phoneshopenight/config/security/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.piseth.java.school.phoneshopenight.config.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	
+
+}
